07ff76eae6727545fb8313b8162387f725619d2a
hyakt <hyakt0@gmail.com>
2021-03-17 02:46:22 +0900

    add: callback function

--- callback/src/index.ts
+++ callback/src/index.ts
@@ -11,13 +11,13 @@ const oauth2Client = new google.auth.OAuth2(
   REDIRECT_URL
 );
 
-const scopes = ["https://www.googleapis.com/auth/calendar.readonly"];
-
-export const auth = (_: Request, res: Response) => {
-  const url = oauth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: scopes,
-  });
-
-  res.send(url);
+export const callback = async (req: Request, res: Response) => {
+  try {
+    const { tokens } = await oauth2Client.getToken(req.query.code as string);
+    res.send(tokens);
+  } catch (err) {
+    res.status(500);
+    console.error(err);
+    res.send("Can not get credentials.");
+  }
 };
--- pipiSchedule/src/index.ts
+++ pipiSchedule/src/index.ts
@@ -1,14 +1,19 @@
+import fs from "fs";
 import { Request, Response } from "express";
 import { google } from "googleapis";
 import fetch from "node-fetch";
 import FormData from "form-data";
 
-const LINE_NOTIFY_TOKEN = process.env.LINE_NOTIFY_TOKEN;
+const fsPromises = fs.promises;
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URL =
-  "https://us-central1-model-fastness-307516.cloudfunctions.net/pipiSchedule";
+const {
+  LINE_NOTIFY_TOKEN,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL,
+} = process.env;
+
+const TOKEN_PATH = "token.json";
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -16,63 +21,66 @@ const oauth2Client = new google.auth.OAuth2(
   REDIRECT_URL
 );
 
-const callback = async (code: string) => {
-  console.log("code", code);
-  const { tokens } = await oauth2Client.getToken(code);
-  oauth2Client.setCredentials(tokens);
+const authorize = async () => {
+  try {
+    const token = await fsPromises.readFile(TOKEN_PATH, "utf-8");
+    oauth2Client.setCredentials(JSON.parse(token));
+  } catch (err) {
+    return Promise.reject("Can not read file.");
+  }
 };
 
 export const pipiSchedule = async (req: Request, res: Response) => {
-  if (req.query.code) {
-    try {
-      await callback(req.query.code as string);
-    } catch (err) {
-      console.error("cant set credentials");
-      console.error("err", err);
-      return;
-    }
+  try {
+    await authorize().catch(() => {
+      res.status(500);
+      res.send("please auhorize first");
+    });
+
+    const calendar = google.calendar({ version: "v3", auth: oauth2Client });
+
+    const result = await calendar.calendarList.get({ calendarId: "primary" });
+    // calendar.events.list(
+    //   {
+    //     calendarId: "primary",
+    //   },
+    //   (err, res) => {
+    //     if (err || !res) return console.log("The API returned an error: " + err);
+    //     const events = res.data.items;
+    //     console.log("events: ", events);
+    //     if (events && events.length) {
+    //       console.log("Upcoming 10 events:");
+
+    //       events.map((event, i) => {
+    //         const start = event.start?.dateTime || event.start?.date;
+    //         console.log(`${start} - ${event.summary}`);
+    //         const params = new FormData();
+    //         params.append("message", `${start} - ${event.summary}`);
+
+    //         fetch("https://notify-api.line.me/api/notify", {
+    //           method: "POST", // or 'PUT'
+    //           headers: {
+    //             Authorization: `Bearer ${LINE_NOTIFY_TOKEN}`,
+    //           },
+    //           body: params,
+    //         })
+    //           .then((response) => response.json())
+    //           .then((data) => {
+    //             console.log("Success:", data);
+    //           })
+    //           .catch((error) => {
+    //             console.error("Error:", error);
+    //           });
+    //       });
+    //     } else {
+    //       console.log("No upcoming events found.");
+    //     }
+    //   }
+
+    // );
+    res.send(result);
+  } catch (err) {
+    res.status(500);
+    res.send("An error occured");
   }
-
-  const calendar = google.calendar({ version: "v3", auth: oauth2Client });
-
-  const result = await calendar.calendarList.get({ calendarId: "primary" });
-  // calendar.events.list(
-  //   {
-  //     calendarId: "primary",
-  //   },
-  //   (err, res) => {
-  //     if (err || !res) return console.log("The API returned an error: " + err);
-  //     const events = res.data.items;
-  //     console.log("events: ", events);
-  //     if (events && events.length) {
-  //       console.log("Upcoming 10 events:");
-
-  //       events.map((event, i) => {
-  //         const start = event.start?.dateTime || event.start?.date;
-  //         console.log(`${start} - ${event.summary}`);
-  //         const params = new FormData();
-  //         params.append("message", `${start} - ${event.summary}`);
-
-  //         fetch("https://notify-api.line.me/api/notify", {
-  //           method: "POST", // or 'PUT'
-  //           headers: {
-  //             Authorization: `Bearer ${LINE_NOTIFY_TOKEN}`,
-  //           },
-  //           body: params,
-  //         })
-  //           .then((response) => response.json())
-  //           .then((data) => {
-  //             console.log("Success:", data);
-  //           })
-  //           .catch((error) => {
-  //             console.error("Error:", error);
-  //           });
-  //       });
-  //     } else {
-  //       console.log("No upcoming events found.");
-  //     }
-  //   }
-  // );
-
-  res.send(result);
 };
